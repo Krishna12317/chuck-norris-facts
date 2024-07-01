8832a09d1c89f01a4b39be975a5e68bc782fcfd4
Krishna12317 <krishna12317@hotmail.com>
2024-07-02 00:10:01 +0530

    chore: updated the app with the context providers,  the components and modified the index.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import React from "react";
+import { ThemeProvider, SearchFactProvider } from "./context";
+import { Header, SearchFact } from "./components/";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  return (
+    <ThemeProvider>
+      <SearchFactProvider>
+        <Header />
+        <SearchFact />
+      </SearchFactProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
