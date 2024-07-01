2a958111bcba7257421d16bab7a759381cd98772
Krishna12317 <krishna12317@hotmail.com>
2024-07-02 02:28:23 +0530

    fix: passed the missing name prop to the button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,8 +3,8 @@ import { StyledButton } from "../styles/StyledButton";
 import { ButtonProps } from "../types";
 
 export const Button: React.FC<ButtonProps> = React.memo(
-  ({ title, onClick, children }) => (
-    <StyledButton title={title} onClick={onClick}>
+  ({ title, onClick, children, name }) => (
+    <StyledButton title={title} name={name} onClick={onClick}>
       {children}
     </StyledButton>
   )
