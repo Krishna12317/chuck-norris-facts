97ba29a904bbc1a8f693bd0dcdd58707f78ce7db
Krishna12317 <krishna12317@hotmail.com>
2024-07-02 00:02:27 +0530

    fix: handled memoizing the spinner loader component and handled typos in it

--- src/components/SpinningLoader.tsx
+++ src/components/SpinningLoader.tsx
@@ -1,12 +1,11 @@
+import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 import { useSearchFactContext, useThemeContext } from "../context";
 
-const SnippingLoader = () => {
+export const SpinningLoader = React.memo(() => {
   const { loading } = useSearchFactContext();
   const { theme } = useThemeContext();
   const loaderColor = theme === "light" ? "#333" : "#ddd";
 
   return <ClipLoader color={loaderColor} loading={loading} size={50} />;
-};
-
-export default SnippingLoader;
+});
